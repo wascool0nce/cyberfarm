be21e896237f318c3eddb19e38f3db279f3aa910
Maxim Demchenko <84056589+wascool0nce@users.noreply.github.com>
2025-11-21 10:30:23 +0300

    Load font from external file

--- internal/adapter/ebitengame/font.go
+++ internal/adapter/ebitengame/font.go
@@ -1,11 +1,13 @@
 package ebitengame
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/golang/freetype/truetype"
 	"golang.org/x/image/font"
-	"golang.org/x/image/font/gofont/goregular"
 )
 
 type Faces struct {
@@ -15,7 +17,12 @@ type Faces struct {
 }
 
 func loadFaces() Faces {
-	fnt, err := truetype.Parse(goregular.TTF)
+	fontBytes, err := loadFontBytes()
+	if err != nil {
+		log.Fatalf("не удалось загрузить файл шрифта CyberMono.ttf: %v", err)
+	}
+
+	fnt, err := truetype.Parse(fontBytes)
 	if err != nil {
 		log.Fatalf("failed to parse font: %v", err)
 	}
@@ -23,8 +30,34 @@ func loadFaces() Faces {
 		return truetype.NewFace(fnt, &truetype.Options{Size: size, DPI: 72})
 	}
 	return Faces{
-		Small:  mk(12),
-		Normal: mk(14),
-		Large:  mk(18),
+		Small:  mk(14),
+		Normal: mk(18),
+		Large:  mk(26),
 	}
+}
+
+func loadFontBytes() ([]byte, error) {
+	searchPaths := []string{
+		filepath.Join("internal", "adapter", "ebitengame", "assets", "CyberMono.ttf"),
+		filepath.Join("assets", "CyberMono.ttf"),
+	}
+
+	if exePath, err := os.Executable(); err == nil {
+		exeDir := filepath.Dir(exePath)
+		searchPaths = append([]string{
+			filepath.Join(exeDir, "assets", "CyberMono.ttf"),
+			filepath.Join(exeDir, "CyberMono.ttf"),
+		}, searchPaths...)
+	}
+
+	var lastErr error
+	for _, path := range searchPaths {
+		data, err := os.ReadFile(path)
+		if err == nil {
+			return data, nil
+		}
+		lastErr = err
+	}
+
+	return nil, fmt.Errorf("файл шрифта CyberMono.ttf не найден; разместите его рядом с бинарником в папке assets или в internal/adapter/ebitengame/assets (последняя ошибка: %v)", lastErr)
 }
